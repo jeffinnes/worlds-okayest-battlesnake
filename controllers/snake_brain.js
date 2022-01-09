a44a1cc70d9d6a31119c5b16d47f0c5909054d29
Jeff Innes <contact.me@jeffinnes.dev>
2022-01-08 22:19:58 -0700

    Add logic to snake's brain so it doesn't leave the board.

--- controllers/snake_brain.js
+++ controllers/snake_brain.js
@@ -25,12 +25,37 @@ function move(req, res) {
 
   // Don't fold my head inside out
   if (my.head.y < my.body[1].y) {
+    // Up
     possibleMoves[0].safe = false;
   } else if (my.head.y > my.body[1].y) {
+    // Down
     possibleMoves[1].safe = false;
   } else if (my.head.x > my.body[1].x) {
+    // Left
     possibleMoves[2].safe = false;
   } else {
+    // Right
+    possibleMoves[3].safe = false;
+  }
+
+  /**
+   * These two work this way be cause if the head is at one edge
+   * it cannot possibly be at the other edge too.
+   * (But could be in a corner which is why I check x and y on every turn)
+   * If this ever changes we will need to check all four ever time.
+   */
+
+  // Don't leave the board through the top or bottom. It's death out there.
+  if (my.head.y === (board.height - 1)) {
+    possibleMoves[0].safe = false;
+  } else if (my.head.y === 0) {
+    possibleMoves[1].safe = false;
+  }
+
+  // Don't leave the board through the left or right. It's death out there.
+  if (my.head.x === 0) {
+    possibleMoves[2].safe = false;
+  } else if (my.head.x === (board.height - 1)) {
     possibleMoves[3].safe = false;
   }
 
